c015f1e9e89f8420695ade531d3f9629ceb93859
Steven Burris <stevenburris.jr@gmail.com>
2016-09-19 17:23:18 -0400

    Just getting started

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+/**
+ * Created by stevenburris on 9/19/16.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        HashMap<String, Tanks> tanks = new HashMap<>();
+
+
+
+
+
+
+    }
+}
---
+++ src/Tanks.java
@@ -0,0 +1,8 @@
+/**
+ * Created by stevenburris on 9/19/16.
+ */
+public class Tanks {
+    String tankNames;
+    boolean hasBeenCleaned;
+
+}
